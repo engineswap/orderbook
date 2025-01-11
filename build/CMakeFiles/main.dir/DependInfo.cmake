601d2a10933c68a807982147d8fb9623781f5228
Ilya <yzybot1@gmail.com>
2025-01-11 14:11:10 -0600

    added cmake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ilyat/Desktop/orderbook/src/helpers.cpp" "CMakeFiles/main.dir/src/helpers.cpp.o" "gcc" "CMakeFiles/main.dir/src/helpers.cpp.o.d"
+  "/Users/ilyat/Desktop/orderbook/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/Users/ilyat/Desktop/orderbook/src/orderbook.cpp" "CMakeFiles/main.dir/src/orderbook.cpp.o" "gcc" "CMakeFiles/main.dir/src/orderbook.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
